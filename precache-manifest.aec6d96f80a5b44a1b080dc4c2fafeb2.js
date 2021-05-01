ed6938e2c2b58a5a2fcf005960bae71c19be096c
Qiuhong Anna Wei <54920263+QiuhongAnnaWei@users.noreply.github.com>
2021-05-02 00:08:42 +0800

    Updates

---
+++ precache-manifest.aec6d96f80a5b44a1b080dc4c2fafeb2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3900c77bada6cf0df0f1ab00e976f6",
+    "url": "qiuhongannawei.me/index.html"
+  },
+  {
+    "revision": "6b4363ab6b02068005fe",
+    "url": "qiuhongannawei.me/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "26bd687b45b832380459",
+    "url": "qiuhongannawei.me/static/js/2.bd6ff9a8.chunk.js"
+  },
+  {
+    "revision": "6b4363ab6b02068005fe",
+    "url": "qiuhongannawei.me/static/js/main.55e1e6af.chunk.js"
+  },
+  {
+    "revision": "4d7e592140d7edbfb156",
+    "url": "qiuhongannawei.me/static/js/runtime~main.4ec54cb5.js"
+  },
+  {
+    "revision": "cbf38067c2ffe62824a123800f607cec",
+    "url": "qiuhongannawei.me/static/media/gooey1.cbf38067.png"
+  },
+  {
+    "revision": "6f86be17f1fe74944e8944dacd93636a",
+    "url": "qiuhongannawei.me/static/media/name1.6f86be17.png"
+  },
+  {
+    "revision": "d50de5ee16d5000ef2f91e17336ef8ee",
+    "url": "qiuhongannawei.me/static/media/profile.d50de5ee.png"
+  },
+  {
+    "revision": "28f560de6ed605abb0212d3d93623832",
+    "url": "qiuhongannawei.me/static/media/toolkit1.28f560de.png"
+  },
+  {
+    "revision": "4502ee30081339b8f4f7479d187837a0",
+    "url": "qiuhongannawei.me/static/media/zoom1.4502ee30.jpg"
+  }
+]);
